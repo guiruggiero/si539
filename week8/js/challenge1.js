19ce29e43f17fda95c3d33dfe1a6cb5a6f00cc8b
Gui Ruggiero <guilherme.ruggiero@gmail.com>
2020-02-27 14:02:18 -0500

    week 8

---
+++ week8/js/challenge1.js
@@ -0,0 +1,14 @@
+subscribe.addEventListener("change", function)
+function(){
+console.log("here");
+var email = document.querySelector("#emailDiv");
+console.log(email);
+console.log(this.checked);
+if (this.checked){
+    email.style.display = "block";
+    email.style.color = "purple";
+}
+else{
+    email.style.display = "none"
+}
+}
